4cb6fb9e7397f41f21783019110caec68b7c82bc
Vadim Zaytsev <lepik@MacBook-Pro-Vadim.local>
2021-03-15 20:52:39 +0400

    First commit - create index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="content">
+        <div class="posts">
+            <h1>Posts</h1>
+            <table class="posts__items">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Theme</th>
+                        <th>Created</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            <img src="" alt="">
+                        </td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td class="posts__buttons">
+                            <a href="/" class="button button_blue">Show</a>
+                            <a href="/" class="button button_green">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
